9f742032f943ca0bc02da24074821eed899dc4fb
vladik110 <119513745+vladik110@users.noreply.github.com>
2022-12-02 21:52:12 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import re
+import sys
+
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+
+
+class Main(QDialog):
+    def __init__(self):
+        super(Main, self).__init__()
+        loadUi('main.ui', self)
+
+        self.setWindowTitle('Работа со строками в Python')
+
+        self.btn_solve.clicked.connect(self.solve)
+        self.btn_clear.clicked.connect(self.clear)
+
+    def solve(self):
+        self.textEdit_words.clear()
+        text = self.textEdit_text.toPlainText().split()
+
+        itog = ""
+
+        for word in text:
+            for i in range(len(word)):
+                if i % 2 == 0:
+                    itog += word[i]
+            itog += " "
+        self.textEdit_words.insertPlainText(itog)
+
+    def clear(self):
+        self.textEdit_text.clear()
+        self.textEdit_words.clear()
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
